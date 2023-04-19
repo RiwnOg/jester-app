11498d975978e39c1e53d61725cf17fc54621d57
RiwnOg <126474223+RiwnOg@users.noreply.github.com>
2023-04-19 17:40:28 -0300

    Super simple page for url test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,12 @@
-import { authOptions } from '@/lib/auth';
-import { formatDate } from '@/lib/utils';
-import Navbar from '@/navbar/Navbar';
-import { Example } from '@prisma/client';
-import { getServerSession } from 'next-auth/next';
-
 function getBaseUrl() {
   return process.env.BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
 }
 
 export default async function Home() {
-  const sessionData = await getServerSession(authOptions);
   const url = getBaseUrl();
 
-  const res = await fetch(`${url}/api/example`, {
-    method: 'GET',
-  });
-  const examples = await res.json();
-
-  return (
-    <div>
-      <Navbar />
-      <main className=''>
-        <div className=''>
-          <h1 className='underline text-2xl'>Welcome! 👋</h1>
-        </div>
-        {sessionData ? (
-          <div>
-            <p>Congrats! {sessionData?.user?.name} 🎉</p>
-            <p>You have Found my secrets</p>
-            {examples &&
-              examples.map((example: Example) => (
-                <li key={example.id}>
-                  <p>Post: {example.post}</p>
-                  <p>Created at: {formatDate(example.createdAt.toString())}</p>
-                </li>
-              ))}
-          </div>
-        ) : (
-          <p>You cant see my secrets </p>
-        )}
-      </main>
-    </div>
-  );
+  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+  console.log(process.env.BASE_URL);
+  console.log(process.env.url);
+  return <p>hello world: {url}</p>;
 }
