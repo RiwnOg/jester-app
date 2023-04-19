a21af913e34d215d79a9d60f904b3782d0fb0cfd
RiwnOg <126474223+RiwnOg@users.noreply.github.com>
2023-04-19 17:43:00 -0300

    Add Base_url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 function getBaseUrl() {
-  return process.env.BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
+  return process.env.BASE_URL ?? process.env.VERCEL_URL;
 }
 
 export default async function Home() {
   const url = getBaseUrl();
 
   console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
+  console.log(process.env.VERCEL_URL);
   console.log(process.env.BASE_URL);
   console.log(process.env.url);
   return <p>hello world: {url}</p>;
