55a1504bdffe22fbdfccca479ca8399971fedbdf
RiwnOg <126474223+RiwnOg@users.noreply.github.com>
2023-04-19 17:35:18 -0300

    Fetch via API using Dynamic URL

---
+++ src/app/api/example/route.ts
@@ -0,0 +1,20 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  // const { searchParams } = new URL(request.url);
+  // const obj = Object.fromEntries(searchParams.entries());
+  // return NextResponse.json(obj);
+
+  // const name = searchParams.get('name');
+  // const instrument = searchParams.get('instrument');
+  // return NextResponse.json({ name, instrument });
+
+  const res = await prisma.example.findMany();
+  return NextResponse.json(res);
+}
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  return NextResponse.json({ res });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import getExample from '@/actions/getExample';
 import { authOptions } from '@/lib/auth';
 import { formatDate } from '@/lib/utils';
 import Navbar from '@/navbar/Navbar';
+import { Example } from '@prisma/client';
 import { getServerSession } from 'next-auth/next';
+
+export function getBaseUrl() {
+  return process.env.BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
+}
 
 export default async function Home() {
   const sessionData = await getServerSession(authOptions);
-  const examples = await getExample();
+  const url = getBaseUrl();
+
+  const res = await fetch(`${url}/api/example`, {
+    method: 'GET',
+  });
+  const examples = await res.json();
 
   return (
     <div>
@@ -20,7 +29,7 @@ export default async function Home() {
             <p>Congrats! {sessionData?.user?.name} 🎉</p>
             <p>You have Found my secrets</p>
             {examples &&
-              examples.map((example) => (
+              examples.map((example: Example) => (
                 <li key={example.id}>
                   <p>Post: {example.post}</p>
                   <p>Created at: {formatDate(example.createdAt.toString())}</p>
