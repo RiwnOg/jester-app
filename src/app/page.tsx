3ebaa924ff6ae16d995cf3c36efb77179a36603e
RiwnOg <126474223+RiwnOg@users.noreply.github.com>
2023-04-19 18:36:30 -0300

    Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,45 @@
+import { authOptions } from '@/lib/auth';
+import { formatDate } from '@/lib/utils';
+import Navbar from '@/navbar/Navbar';
+import { Example } from '@prisma/client';
+import { getServerSession } from 'next-auth/next';
+
 function getBaseUrl() {
-  return process.env.BASE_URL ?? process.env.VERCEL_URL;
+  return process.env.BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL;
 }
 
 export default async function Home() {
+  const sessionData = await getServerSession(authOptions);
   const url = getBaseUrl();
+
+  const res = await fetch(`${url}/api/example`, {
+    method: 'GET',
+  });
+  const examples = await res.json();
 
   return (
     <div>
-      <p>hello world - new branch </p>
-      <p>NEXT_PUBLIC_SITE_URL: ~{process.env.NEXT_PUBLIC_SITE_URL}~ </p>
-      <p>NEXT_PUBLIC_VERCEL_URL: ~{process.env.NEXT_PUBLIC_VERCEL_URL}~ </p>
-      <p>VERCEL_URL: ~{process.env.VERCEL_URL}~</p>
-      <p>BASE_URL: ~{process.env.BASE_URL}~</p>
-      <p>url: ~{process.env.url}~</p>
+      <Navbar />
+      <main className=''>
+        <div className=''>
+          <h1 className='underline text-2xl'>Welcome! 👋</h1>
+        </div>
+        {sessionData ? (
+          <div>
+            <p>Congrats! {sessionData?.user?.name} 🎉</p>
+            <p>You have Found my secrets</p>
+            {examples &&
+              examples.map((example: Example) => (
+                <li key={example.id}>
+                  <p>Post: {example.post}</p>
+                  <p>Created at: {formatDate(example.createdAt.toString())}</p>
+                </li>
+              ))}
+          </div>
+        ) : (
+          <p>You cant see my secrets </p>
+        )}
+      </main>
     </div>
   );
 }
